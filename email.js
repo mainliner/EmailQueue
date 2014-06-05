298352e53373d579a8a05524e52a113c581d4cd0
Song <nosongyang@gmail.com>
2014-06-05 14:53:56 +0800

    add user and passwd for rabbitmq

--- email.js
+++ email.js
@@ -9,7 +9,10 @@ var _log = function(msg) {
 
 var amqp = require('amqp');
 
-var connection = amqp.createConnection({ host: settings.rabbitmq, port: settings.rabbitmqPort });
+var connection = amqp.createConnection({ host: settings.rabbitmq,
+					 port: settings.rabbitmqPort,
+					 login: settings.rabbitmqUser,
+					 password: settings.rabbitmqPassword});
 
 connection.on('ready',function(){
     console.log('connect to the Email Queue');
--- settings.js
+++ settings.js
@@ -1,6 +1,8 @@
 module.exports = {
     rabbitmq:'localhost',
     rabbitmqPort:5672,
+    rabbitmqUser:'guest',
+    rabbitmqPassword:'guest',
     queue: 'Email',
     exchange: 'router',
     routingKey: 'E'
